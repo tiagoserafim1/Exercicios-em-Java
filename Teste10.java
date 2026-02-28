56a3b7287adab98d730390787b5dce5948165f0e
Tiago Serafim de Souza <tiagosouza336@outlook.com>
2026-02-28 20:17:49 -0300

    Add files via upload

---
+++ Teste10.java
@@ -0,0 +1,20 @@
+public class Teste10 {
+    public static void main(String[] args) {
+        Personagem p1 = new Personagem();
+        p1.vida = 100;
+        p1.nome = "Goku";
+        p1.ataque = 20;
+        Personagem p2 = new Personagem();
+        p2.vida = 100;
+        p2.nome = "Vegeta";
+        p2.ataque = 15;
+        Boss p3 = new Boss();
+        p3.vida = 500;
+        p3.nome = "Freeza";
+        p3.ataque = 25;
+        /*System.out.println(p1.nome + " tem " + p1.vida + " de vida.");
+        System.out.println(p2.nome + " tem " + p2.vida + " de vida.");
+        System.out.println(p3.nome + " tem " + p3.vida + " de vida.");
+        int golpe1 = p3.vida - p1.ataque;***/
+    }
+}
