fd862fb2e6d3319b8761ba57e1809df88f7d2739
Tiago Serafim de Souza <tiagosouza336@outlook.com>
2026-02-28 20:18:18 -0300

    Update and rename teste1.java to Teste1.java

--- Teste1.java
+++ Teste1.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-public class teste1 {
+public class Teste1 {
     public static void main(String[] args) {
         System.out.println("Qual seu nome?");
         Scanner sc = new Scanner(System.in);
         String nome = sc.nextLine();
         System.out.println("olá, " + nome + "!");
     }
+
 }
