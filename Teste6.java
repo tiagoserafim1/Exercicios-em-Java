c79bc18ebf0bad4d15e494b174f4960b591acca0
Tiago Serafim de Souza <tiagosouza336@outlook.com>
2026-03-14 20:13:27 -0300

    Update and rename teste6.java to Teste6.java

--- Teste6.java
+++ Teste6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class teste6 {
+public class Teste6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite quantos números quer somar:");
